2e08921ac0304d5af6049342727a56b22cabcf78
Luis Guilherme <luis-guilherme.pereira@unesp.br>
2021-07-21 01:41:15 -0300

    fixed card bug

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "da5036d2dcc5346b31e5"; }
+/******/ 		__webpack_require__.h = function() { return "68b950a3913c7d279bb2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
